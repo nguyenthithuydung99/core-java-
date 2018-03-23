a9a9ed4df7ad774bb0b30704386e21bf76c0f076
nguyenthithuydung99 <37462186+nguyenthithuydung99@users.noreply.github.com>
2018-03-23 12:22:27 +0700

    Update Bai1.java

--- Basic Exercises [ 117 Exercises with Solution ]/Bai1.java
+++ Basic Exercises [ 117 Exercises with Solution ]/Bai1.java
@@ -4,7 +4,7 @@ public class Bai1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello");
-		System.out.print("Le Tri Nhan");
+		System.out.print("Nguyen Thi Thuy Dung");
 	}
 
 }
